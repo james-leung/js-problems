f3d12272d80e29d9e51d3a5ab07dd7540d52a9ad
James Leung <jlfly12@gmail.com>
2020-08-09 10:09:42 -0400

    word ladder

--- word-ladder.js
+++ word-ladder.js
@@ -1,19 +1,22 @@
 function ladderLength(beginWord, endWord, wordList) {
-  set = {};
-  q = [];
-  q.push(beginWord);
+  set = new Set(wordList);
+  q = [beginWord];
   let step = 1;
   while (q.length > 0) {
     let size = q.length;
     for (let j = 0; j < size; j++) {
-      let cur = q.pop();
-      for (let i = 0; i < endWord.length(); i++) {
-        for (let letter = "a"; letter <= "z"; letter++) {
+      let cur = q.shift();
+      for (let i = 0; i < endWord.length; i++) {
+        for (
+          let letter = "a";
+          letter <= "z";
+          letter = String.fromCharCode(letter.charCodeAt(0) + 1)
+        ) {
           let newWord = cur;
-          newWord[i] = letter;
-          if (set[newWord]) {
+          newWord = newWord.substring(0,i) + letter + newWord.substring(i+1);
+          if (set.has(newWord)) {
             if (newWord === endWord) return step + 1;
-            set[newWord] = null;
+            set.delete(newWord);
             q.push(newWord);
           }
         }
@@ -23,3 +26,9 @@ function ladderLength(beginWord, endWord, wordList) {
   }
   return 0;
 }
+
+let beginWord = "hit";
+let endWord = "cog";
+let wordList = ["hot", "dot", "dog", "lot", "log", "cog"];
+
+console.log(ladderLength(beginWord, endWord, wordList));
